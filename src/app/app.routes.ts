5f228c72dc0c26c61770a02b9e93f8cfcb032170
Quentin Horgues <quentin.horgues@etu.univ-tours.fr>
2025-11-14 15:25:23 +0100

    add routes and not found components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: "", component: HomeComponent },
+  // { path: "users/new", component: NewUserComponent },
+  // { path: "users/:id", component: UserDetailComponent },
+  // { path: "users", component: UsersComponent },
+  { path: "**", component: NotFoundComponent },
+];
