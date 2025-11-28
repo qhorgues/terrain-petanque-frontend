2f31fb437e0d6c903ab91272f640181b50888092
Quentin Horgues <quentin.horgues@etu.univ-tours.fr>
2025-11-28 15:57:36 +0100

    menu for desktop

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,13 +1,44 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { MatSidenav } from "@angular/material/sidenav";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { BreakpointObserver } from "@angular/cdk/layout";
 
 @Component({
   selector: "app-menu",
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, RouterLink],
   templateUrl: "./menu.component.html",
   styleUrl: "./menu.component.css",
 })
-export class MenuComponent {}
+export class MenuComponent {
+  @ViewChild("sidenav") sidenav!: MatSidenav;
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
+
+  closeSidenav() {
+    this.sidenav.close();
+  }
+
+  openSidenav() {
+    this.sidenav.open();
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe(["(max-width: 768px)"])
+      .subscribe((result) => {
+        if (result.matches) {
+          this.sidenav.mode = "over";
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = "side";
+          this.sidenav.open();
+        }
+      });
+  }
+}
