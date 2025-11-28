c4d839962b7651f1e3f9feb37ecc98a0543bd025
Alexis Guillotin <alexis.guillotin@etu.univ-tours.fr>
2025-11-28 15:24:29 +0100

    add login page to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from "@angular/router";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { LoginComponent } from "./pages/login/login.component";
 
 export const routes: Routes = [
   // { path: "", component: HomeComponent },
   // { path: "users/new", component: NewUserComponent },
   // { path: "users/:id", component: UserDetailComponent },
   // { path: "users", component: UsersComponent },
+  { path: "login", component: LoginComponent },
   { path: "**", component: NotFoundComponent },
 ];
