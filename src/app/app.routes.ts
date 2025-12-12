ee1f326d15eb68e82d562d839b20e94a6d8515a8
Quentin Horgues <quentin.horgues@etu.univ-tours.fr>
2025-12-12 15:58:23 +0100

    Test map

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from "@angular/router";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { LoginComponent } from "./pages/login/login.component";
+import { MapPage } from "./pages/map/map.page";
 
 export const routes: Routes = [
   // { path: "", component: HomeComponent },
   // { path: "users/new", component: NewUserComponent },
   // { path: "users/:id", component: UserDetailComponent },
   // { path: "users", component: UsersComponent },
+  { path: "map", component: MapPage },
   { path: "login", component: LoginComponent },
   { path: "**", component: NotFoundComponent },
 ];
