2eb85b31ee7e224f410d047ebfc809e5a87aef74
Anthony Gimenez <anthony.gimenez@etu.univ-tours.fr>
2025-12-29 22:48:15 +0100

    Add the connection with the service

--- src/app/components/password-field/password-field.component.ts
+++ src/app/components/password-field/password-field.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation} from '@angular/core';
+import { Component, ViewEncapsulation, output} from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
@@ -8,10 +9,17 @@ import {MatInputModule} from '@angular/material/input';
   encapsulation: ViewEncapsulation.None,
   selector: 'app-password-field',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './password-field.component.html',
   styleUrl: './password-field.component.css'
 })
 export class PasswordFieldComponent {
   hide = true;
+
+  readonly form = new FormControl('');
+  password = output<string | null>();
+
+  updatePassword() {
+    this.password.emit(this.form.value);
+  }
 }
