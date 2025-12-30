976c2ca012735e31ca5d289349ef0f561813de40
Anthony Gimenez <anthony.gimenez@etu.univ-tours.fr>
2025-12-31 00:28:26 +0100

    Add refresh to the login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EmailFieldComponent } from "../../components/email-field/email-field.component";
 import { PasswordFieldComponent } from "../../components/password-field/password-field.component";
 import { ConfirmButtonComponent } from "../../components/confirm-button/confirm-button.component";
 import { UserInputInterface } from '../../interfaces/input/userInputInterface';
 import { UserService } from '../../services/userService/user.service';
 import { FormsModule  } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,10 +14,21 @@ import { FormsModule  } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   credential: UserInputInterface = {name: "", surname : "", mail : "", password : "", username : ""};
 
-  constructor(private userService : UserService) {}
+  constructor(private userService : UserService, private authService : AuthService) {}
+
+  /**
+   * This method force the connected user to unacces to this page (and return to the home page when is connected).
+   */
+  ngOnInit() : void {
+    setInterval(() => {
+      if (this.authService.isLoggedIn()) {
+        window.location.href = "/"; //This force to reload all the website. So the user is now connected and the menu is the correct menu.
+      }
+    }, 500);
+  }
 
   updateMail(email : string | null) {
     if (email == null) return;
